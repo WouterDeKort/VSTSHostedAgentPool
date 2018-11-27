e7215d37fdac034f24f92c0bab9ed385d376b8fa
Anton Kuryan <anton.kuryan@colours.nl>
2018-11-27 18:26:19 +0300

    Added test cases for retrieving data from API

--- autoscalingApp/AgentsMonitor/Tests/AzureDevOps.Operations.Tests/Classes/RetrieveTests.cs
+++ autoscalingApp/AgentsMonitor/Tests/AzureDevOps.Operations.Tests/Classes/RetrieveTests.cs
@@ -9,34 +9,54 @@ namespace AzureDevOps.Operations.Tests.Classes
 {
     public class RetrieveTests
     {
-        [TestCase(@"..\..\Data\TestData\GetPoolId\pools-success.json")]
+        [TestCase(@"..\..\Data\TestData\GetPoolId\pools-success.json", Description = "Finds pool id by name successfully")]
         public void GetPoolIdTest_Pool_Present(string jsonPath)
         {
-            var client = SetupHttpClient(jsonPath);
-
-            var dataRetriever = new Retrieve(TestsConstants.TestOrganizationName, TestsConstants.TestToken,
-                client);
+            var dataRetriever = CreateRetriever(jsonPath);
 
             var poolId = dataRetriever.GetPoolId(TestsConstants.TestPoolName);
 
             Assert.IsNotNull(poolId);
-            Assert.AreEqual(poolId.Value, 12);
+            Assert.AreEqual(poolId.Value, TestsConstants.TestPoolId);
         }
 
-        [TestCase(@"..\..\Data\TestData\GetPoolId\pools-fail.json")]
+        [TestCase(@"..\..\Data\TestData\GetPoolId\pools-fail.json", Description = "There is no pool with required name")]
+        [TestCase(@"\fileNotExists", Description = "Response was with status 200, but empty")]
         public void GetPoolIdTest_Pool_Not_Present(string jsonPath)
         {
-            var client = SetupHttpClient(jsonPath);
-
-            var dataRetriever = new Retrieve(TestsConstants.TestOrganizationName, TestsConstants.TestToken,
-                client);
+            var dataRetriever = CreateRetriever(jsonPath);
 
             var poolId = dataRetriever.GetPoolId(TestsConstants.TestPoolName);
 
             Assert.IsNull(poolId);
         }
 
-        private static HttpClient SetupHttpClient(string jsonPathResponse)
+        [TestCase(@"..\..\Data\TestData\Agents\allAgents.json", Description = "Check json parsing for agents")]
+        public void CheckAgentsRetrieval(string jsonPath)
+        {
+            var dataRetriever = CreateRetriever(jsonPath);
+
+            var allAgents = dataRetriever.GetAllAccessibleAgents(TestsConstants.TestPoolId);
+            var onlineAgents = dataRetriever.GetOnlineAgentsCount(TestsConstants.TestPoolId);
+
+            Assert.IsNotNull(allAgents);
+            Assert.AreEqual(allAgents, TestsConstants.AllAgentsCount);
+            Assert.IsNotNull(onlineAgents);
+            Assert.AreEqual(onlineAgents.Value, TestsConstants.OnlineAgentsCount);
+        }
+
+        [TestCase(@"..\..\Data\TestData\JobRequests\jobs-0-running.json", 0, Description = "There is 0 jobs running according to test JSON")]
+        [TestCase(@"..\..\Data\TestData\JobRequests\jobs-1-running.json", 1, Description = "There is 1 job running according to test JSON")]
+        public void CheckJobsRetrieval(string jsonPath, int runningJobs)
+        {
+            var dataRetriever = CreateRetriever(jsonPath);
+
+            var jobsRunning = dataRetriever.GetCurrentJobsRunning(TestsConstants.TestPoolId);
+
+            Assert.AreEqual(jobsRunning, runningJobs);
+        }
+
+        private static Retrieve CreateRetriever(string jsonPathResponse)
         {
             var mockHttp = new MockHttpMessageHandler();
 
@@ -45,7 +65,9 @@ namespace AzureDevOps.Operations.Tests.Classes
             var response = File.Exists(jsonPathCombined) ? File.ReadAllText(jsonPathCombined) : string.Empty;
 
             var request = mockHttp.When("*").Respond("application/json", response);
-            return mockHttp.ToHttpClient();
+            var client = mockHttp.ToHttpClient();
+            return new Retrieve(TestsConstants.TestOrganizationName, TestsConstants.TestToken,
+                client);
         }
     }
 }
--- autoscalingApp/AgentsMonitor/Tests/AzureDevOps.Operations.Tests/Data/TestsConstants.cs
+++ autoscalingApp/AgentsMonitor/Tests/AzureDevOps.Operations.Tests/Data/TestsConstants.cs
@@ -5,5 +5,9 @@
         internal const string TestOrganizationName = "testOrganization";
         internal const string TestToken = "testToken";
         internal const string TestPoolName = "testPool";
+
+        internal const int TestPoolId = 12;
+        internal const int AllAgentsCount = 4;
+        internal const int OnlineAgentsCount = 3;
     }
 }
