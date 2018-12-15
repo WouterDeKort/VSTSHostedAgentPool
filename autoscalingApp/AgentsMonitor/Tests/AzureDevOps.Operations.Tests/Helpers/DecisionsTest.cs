88f9eb526156c05707e1cc7cd2072a7b200f9a63
Anton Kuryan <anton.kuryan@colours.nl>
2018-12-15 15:34:52 +0300

    Added test cases

--- autoscalingApp/AgentsMonitor/Tests/AzureDevOps.Operations.Tests/Helpers/DecisionsTest.cs
+++ autoscalingApp/AgentsMonitor/Tests/AzureDevOps.Operations.Tests/Helpers/DecisionsTest.cs
@@ -1,10 +1,13 @@
-﻿using AzureDevOps.Operations.Helpers;
+﻿using System;
+using AzureDevOps.Operations.Helpers;
 using AzureDevOps.Operations.Models;
 using AzureDevOps.Operations.Tests.Classes;
 using AzureDevOps.Operations.Tests.Data;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using AzureDevOps.Operations.Classes;
 using Microsoft.Azure.Management.Compute.Fluent;
 
 namespace AzureDevOps.Operations.Tests.Helpers
@@ -22,11 +25,33 @@ namespace AzureDevOps.Operations.Tests.Helpers
 
         [TestCase(3, 3, 7, 0, Description = "There is 3 jobs and 3 agents - do not need to add anything")]
         [TestCase(5, 3, 7, 2, Description = "There is 5 jobs and 3 agents - need 2 more agents")]
-        [TestCase(1, 3, 7, 2, Description = "There is 1 job and 3 agents - could deprovision 2 agents")]
+        [TestCase(1, 3, 7, 2, Description = "There is 1 job and 3 agents - shall deprovision 2 agents")]
         [TestCase(10, 7, 7, 0, Description = "There is 10 jobs and 7 agents, with 7 agents max - could not do anything")]
         [TestCase(7, 7, 7, 0, Description = "There is 7 jobs and 7 agents, with 7 agents max - should not do anything")]
         public static void AmountOfAgents(int jobsCount, int agentsCount, int maxAgentsCount, int expectedAmount)
         {
+            var amount = Decisions.HowMuchAgents(jobsCount, agentsCount, maxAgentsCount);
+
+            Assert.AreEqual(amount, expectedAmount);
+        }
+
+        [TestCase(3, 3, 7, "15-Dec-2018 15:15", 0, Description = "There is 3 jobs and 3 agents at Saturday 15:15 - do not need to add anything")]
+        [TestCase(5, 3, 7, "15-Dec-2018 15:15", 2, Description = "There is 5 jobs and 3 agents at Saturday 15:15 - need 2 more agents")]
+        [TestCase(1, 3, 7, "15-Dec-2018 15:15", 2, Description = "There is 1 job and 3 agents at Saturday 15:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "14-Dec-2018 15:15", 0, Description = "There is 1 job and 3 agents at Friday 15:15 - should not do anything")]
+        [TestCase(1, 3, 7, "14-Dec-2018 18:15", 2, Description = "There is 1 job and 3 agents at Friday 18:15 - shall deprovision 2 agents")]
+        [TestCase(10, 7, 7, "14-Dec-2018 15:15", 0, Description = "There is 10 jobs and 7 agents at Friday 15:15, with 7 agents max - could not do anything")]
+        [TestCase(10, 7, 7, "14-Dec-2018 18:15", 0, Description = "There is 10 jobs and 7 agents at Friday 18:15, with 7 agents max - could not do anything")]
+        [TestCase(10, 7, 7, "15-Dec-2018 15:15", 0, Description = "There is 10 jobs and 7 agents at Saturday 15:15, with 7 agents max - could not do anything")]
+        [TestCase(7, 7, 7, "14-Dec-2018 15:15", 0, Description = "There is 7 jobs and 7 agents at Friday 15:15, with 7 agents max - should not do anything")]
+        [TestCase(7, 7, 7, "14-Dec-2018 18:15", 0, Description = "There is 7 jobs and 7 agents at Friday 18:15, with 7 agents max - should not do anything")]
+        [TestCase(7, 7, 7, "15-Dec-2018 15:15", 0, Description = "There is 7 jobs and 7 agents at Saturday 15:15, with 7 agents max - should not do anything")]
+        public static void AmountOfAgentsWithBusinessTimesSet(int jobsCount, int agentsCount, int maxAgentsCount, string currentDateTime, int expectedAmount)
+        {
+            ConfigurationManager.AppSettings[Constants.BusinessHoursRangeSettingName] = "10-17";
+            ConfigurationManager.AppSettings[Constants.BusinessHoursDaysSettingName] = "Mon-Fri";
+            ConfigurationManager.AppSettings[Constants.BusinessHoursAgentsAmountSettingName] = "3";
+
             var amount = Decisions.HowMuchAgents(jobsCount, agentsCount, maxAgentsCount);
 
             Assert.AreEqual(amount, expectedAmount);
