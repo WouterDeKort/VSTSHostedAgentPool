1db32aa4631f3c6460bfe75245193bb6352dd53c
Anton Kuryan <anton.kuryan@colours.nl>
2018-12-15 10:04:04 +0300

    Debugging helpers

--- functions/helpers.psm1
+++ functions/helpers.psm1
@@ -4,9 +4,21 @@ function SetCustomTagOnResource {
     )
 
     process {
-        Write-Verbose "Starting tags settings";
-        $azureResourceInfo = Get-AzureRmResource -ResourceId $resourceId;
-        Set-AzureRmResource -Tag @{ billingCategory="DevProductivity"; environment="Dev"; resourceType="AzureDevOps" } -ResourceName $azureResourceInfo.ResourceName -ResourceType $azureResourceInfo.resourceType -ResourceGroupName $azureResourceInfo.ResourceGroupName -Force;
+        Write-Verbose "Starting tags settings for resource $resourceId";
+        $azureResourceInfo = Get-AzureRmResource -ResourceId $resourceId -ev resourceNotPresent -ea 0;
+        #do not why, but resource retrieval fails sometimes
+        if ($resourceNotPresent) {
+            Write-Verbose "Could not get resource for $resourceId";
+        } 
+        else 
+        {
+            $rName = $azureResourceInfo.ResourceName;
+            $rType = $azureResourceInfo.resourceType;
+            $rRgName = $azureResourceInfo.ResourceGroupName;
+            Write-Verbose "Settings tags for $resourceId named $rName, belonging to type $rType in resource group $rRgName";
+            Set-AzureRmResource -Tag @{ billingCategory="DevProductivity"; environment="Dev"; resourceType="AzureDevOps" } -ResourceName $rName -ResourceType $rType -ResourceGroupName $rRgName -Force;
+        }
+
         Write-Verbose "Ended tags settings"
     }
 }
