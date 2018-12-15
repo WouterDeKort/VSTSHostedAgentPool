3d58d38f2ac3bac9852864f703b2c8cfcd132a3f
Anton Kuryan <anton.kuryan@colours.nl>
2018-12-15 15:41:45 +0300

    Extended test cases to be covered on all week days

--- autoscalingApp/AgentsMonitor/Tests/AzureDevOps.Operations.Tests/Helpers/DecisionsTest.cs
+++ autoscalingApp/AgentsMonitor/Tests/AzureDevOps.Operations.Tests/Helpers/DecisionsTest.cs
@@ -38,8 +38,42 @@ namespace AzureDevOps.Operations.Tests.Helpers
         [TestCase(3, 3, 7, "15-Dec-2018 15:15", 0, Description = "There is 3 jobs and 3 agents at Saturday 15:15 - do not need to add anything")]
         [TestCase(5, 3, 7, "15-Dec-2018 15:15", 2, Description = "There is 5 jobs and 3 agents at Saturday 15:15 - need 2 more agents")]
         [TestCase(1, 3, 7, "15-Dec-2018 15:15", 2, Description = "There is 1 job and 3 agents at Saturday 15:15 - shall deprovision 2 agents")]
+        //Monday test case
+        [TestCase(1, 3, 7, "10-Dec-2018 09:15", 2, Description = "There is 1 job and 3 agents at Monday 09:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "10-Dec-2018 10:15", 0, Description = "There is 1 job and 3 agents at Monday 10:15 - should not do anything")]
+        [TestCase(1, 3, 7, "10-Dec-2018 15:15", 0, Description = "There is 1 job and 3 agents at Monday 15:15 - should not do anything")]
+        [TestCase(1, 3, 7, "10-Dec-2018 18:15", 2, Description = "There is 1 job and 3 agents at Monday 18:15 - shall deprovision 2 agents")]
+        //Tuesday test case
+        [TestCase(1, 3, 7, "11-Dec-2018 09:15", 2, Description = "There is 1 job and 3 agents at Tuesday 09:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "11-Dec-2018 10:15", 0, Description = "There is 1 job and 3 agents at Tuesday 10:15 - should not do anything")]
+        [TestCase(1, 3, 7, "11-Dec-2018 15:15", 0, Description = "There is 1 job and 3 agents at Tuesday 15:15 - should not do anything")]
+        [TestCase(1, 3, 7, "11-Dec-2018 18:15", 2, Description = "There is 1 job and 3 agents at Tuesday 18:15 - shall deprovision 2 agents")]
+        //Wednesday test case
+        [TestCase(1, 3, 7, "12-Dec-2018 09:15", 2, Description = "There is 1 job and 3 agents at Wednesday 09:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "12-Dec-2018 10:15", 0, Description = "There is 1 job and 3 agents at Wednesday 10:15 - should not do anything")]
+        [TestCase(1, 3, 7, "12-Dec-2018 15:15", 0, Description = "There is 1 job and 3 agents at Wednesday 15:15 - should not do anything")]
+        [TestCase(1, 3, 7, "12-Dec-2018 18:15", 2, Description = "There is 1 job and 3 agents at Wednesday 18:15 - shall deprovision 2 agents")]
+        //Thursday test case
+        [TestCase(1, 3, 7, "13-Dec-2018 09:15", 2, Description = "There is 1 job and 3 agents at Thursday 09:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "13-Dec-2018 10:15", 0, Description = "There is 1 job and 3 agents at Thursday 10:15 - should not do anything")]
+        [TestCase(1, 3, 7, "13-Dec-2018 15:15", 0, Description = "There is 1 job and 3 agents at Thursday 15:15 - should not do anything")]
+        [TestCase(1, 3, 7, "13-Dec-2018 18:15", 2, Description = "There is 1 job and 3 agents at Thursday 18:15 - shall deprovision 2 agents")]
+        //Friday test case
+        [TestCase(1, 3, 7, "14-Dec-2018 09:15", 2, Description = "There is 1 job and 3 agents at Friday 09:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "14-Dec-2018 10:15", 0, Description = "There is 1 job and 3 agents at Friday 10:15 - should not do anything")]
         [TestCase(1, 3, 7, "14-Dec-2018 15:15", 0, Description = "There is 1 job and 3 agents at Friday 15:15 - should not do anything")]
         [TestCase(1, 3, 7, "14-Dec-2018 18:15", 2, Description = "There is 1 job and 3 agents at Friday 18:15 - shall deprovision 2 agents")]
+        //Saturday test case
+        [TestCase(1, 3, 7, "15-Dec-2018 09:15", 2, Description = "There is 1 job and 3 agents at Saturday 09:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "15-Dec-2018 10:15", 2, Description = "There is 1 job and 3 agents at Saturday 10:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "15-Dec-2018 15:15", 2, Description = "There is 1 job and 3 agents at Saturday 15:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "15-Dec-2018 18:15", 2, Description = "There is 1 job and 3 agents at Saturday 18:15 - shall deprovision 2 agents")]
+        //Sunday test case
+        [TestCase(1, 3, 7, "16-Dec-2018 09:15", 2, Description = "There is 1 job and 3 agents at Sunday 09:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "16-Dec-2018 10:15", 2, Description = "There is 1 job and 3 agents at Sunday 10:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "16-Dec-2018 15:15", 2, Description = "There is 1 job and 3 agents at Sunday 15:15 - shall deprovision 2 agents")]
+        [TestCase(1, 3, 7, "16-Dec-2018 18:15", 2, Description = "There is 1 job and 3 agents at Sunday 18:15 - shall deprovision 2 agents")]
+        //other test cases
         [TestCase(10, 7, 7, "14-Dec-2018 15:15", 0, Description = "There is 10 jobs and 7 agents at Friday 15:15, with 7 agents max - could not do anything")]
         [TestCase(10, 7, 7, "14-Dec-2018 18:15", 0, Description = "There is 10 jobs and 7 agents at Friday 18:15, with 7 agents max - could not do anything")]
         [TestCase(10, 7, 7, "15-Dec-2018 15:15", 0, Description = "There is 10 jobs and 7 agents at Saturday 15:15, with 7 agents max - could not do anything")]
